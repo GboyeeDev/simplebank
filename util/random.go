fff7a7a848715b712bf14c032f644548c1d8cf24
GboyeeDev <olugboyega.ojelabi@gmail.com>
2023-12-20 09:46:01 +0100

    Working with database postgres & sqlc

---
+++ util/random.go
@@ -0,0 +1,48 @@
+package util
+
+import (
+	"math/rand"
+	"strings"
+	"time"
+)
+
+const alpahabet = "abcdefghijklmnopqrstuvwxyz"
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+// RandomInt generate a random integer between min & max
+func RandomInt(min, max int64) int64 {
+	return min + rand.Int63n(max-min+1)
+}
+
+// RandomString generate a random string of length n
+func RandomString(n int) string {
+	var sb strings.Builder
+	k := len(alpahabet)
+
+	for i := 0; i < n; i++ {
+		c := alpahabet[rand.Intn(k)]
+		sb.WriteByte(c)
+	}
+
+	return sb.String()
+}
+
+// RandomOwner generates a random owner name
+func RandomOwner() string {
+	return RandomString(6)
+}
+
+// RandomMoney generates a random amount of money
+func RandomMoney() int64 {
+	return RandomInt(0, 1000)
+}
+
+// RandomCurrency generates a random
+func RandomCurrency() string {
+	currencies := []string{"EUR", "USD", "CAD"}
+	n := len(currencies)
+	return currencies[rand.Intn(n)]
+}
